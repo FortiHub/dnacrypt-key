9b359b6af48b666d516892684269f1fa755d519d
FortiHub <96688716+FortiHub@users.noreply.github.com>
2022-08-21 21:42:53 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name = 'dnacrypt-key',
+    version = '1.0.0',
+    author = 'Rodrigo Forti',
+    author_email = 'rodrigofort14@gmail.com',
+    packages = ['dnacrypt-key'],
+    description = "A good code for encryption based on the genetic code of DNA",
+    long_description = 'A good code for encryption based on the genetic code of DNA '
+                        + 'you can also use to decrypt too ',
+    url = 'https://github.com/FortiHub/dnacrypt-key',
+    project_urls = {
+        'Código fonte': 'https://github.com/FortiHub/dnacrypt-key',
+        'Download': 'https://github.com/FortiHub/dnacrypt-key/archive/refs/heads/main.zip'
+    },
+    license = 'MIT',
+    keywords = 'Encrypt based DNA code with security key',
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: Portuguese (Brazilian)',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Scientific/Engineering :: Physics'
+    ]
+)
+
